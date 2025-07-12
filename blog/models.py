c3bf3957b5d3d9be0a2d9de4932662a2029d9cb8
ksstrat <ksstratmann@gmail.com>
2025-07-12 21:05:51 +0200

    feat: add post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True) # Title (unique) of the Blog post
+    slug = models.SlugField(max_length=200, unique=True) # URL shorthand for URL generation
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts" # Author is the posting User
+    )
+    content = models.TextField() # The blog article content
+    created_on = models.DateTimeField(auto_now_add=True) # The timestamp
+    status = models.IntegerField(choices=STATUS, default=0) # Status of the blog post - status is constant as defined - 0 = draft - 1 = published
