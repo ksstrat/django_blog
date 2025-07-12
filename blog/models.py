883cdd1ff7c87ded199d81001ff4ab59df275d4f
ksstrat <ksstratmann@gmail.com>
2025-07-12 22:59:42 +0200

    feat: add comment model

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,15 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0) # Status of the blog post - status is constant as defined - 0 = draft - 1 = published
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter" # Author is the posting User
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
