f4fe0ee76d63532ca8496319b5b4460efc8eb8d6
ksstrat <ksstratmann@gmail.com>
2025-07-16 21:22:55 +0200

    feat: add comments display to the blockpost

--- blog/views.py
+++ blog/views.py
@@ -10,24 +10,18 @@ class PostList(generic.ListView):
 
 
 def post_detail(request, slug):
-    """
-    Display an individual :model:`blog.Post`.
-
-    **Context**
-
-    ``post``
-        An instance of :model:`blog.Post`.
-
-    **Template:**
-
-    :template:`blog/post_detail.html`
-    """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
 
 
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post,})
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+         },
+    )
